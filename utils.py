51fe31615675d6810d759ed58daf2a79998b52af
yanishk <yanis.harkouk@epitech.eu>
2025-01-31 11:32:06 +0100

    update utils functions

--- utils.py
+++ utils.py
@@ -1,8 +1,21 @@
+import json
+
 def get_file_content(file_path):
     file = open(file_path, 'r')
     content = file.read()
     return content
 
+def get_json_file_content(file_path):
+    file = open("data/dictionary/morphalouV3.json", 'r')
+    content = json.load(file)
+    return content
+
 def write_content_in_file(file_path, content, open_mode):
     file = open(file_path, open_mode)
     file.write(content)
+
+def string_contains_digit(string):
+    for char in string:
+            if char.isdigit():
+                    return True
+    return False
