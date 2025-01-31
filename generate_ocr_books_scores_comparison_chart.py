ff45fd383c38c4db68a74725708bef95425e1bc2
yanishk <yanis.harkouk@epitech.eu>
2025-01-31 11:28:41 +0100

    improve ocr books scores comparison chart visual

--- generate_ocr_books_scores_comparison_chart.py
+++ generate_ocr_books_scores_comparison_chart.py
@@ -4,24 +4,25 @@ import textwrap
 from sort_data_related_to_ocr_books_scores import books_name, kraken_scores, paddleocr_scores, tesseract_scores
 
 def generate_ocr_books_scores_comparison_chart(books_name, kraken_scores, paddleocr_scores, tesseract_scores):
-    print("generating ocr books scores comparison chart...")
+    print("Generating OCR Accuracy Scores by Book and Framework Chart...")
 
-    plt.figure("OCR scores comparison chart")
+    plt.figure("OCR Accuracy Scores by Book and Framework", figsize=(10, len(books_name) * 0.5))
+
     bars_width = 0.25
-    kraken_bar_x_pos = np.arange(len(kraken_scores))
-    paddleocr_bar_x_pos = np.arange(len(paddleocr_scores)) + (bars_width * 1)
-    tesseract_bar_x_pos = np.arange(len(tesseract_scores)) + (bars_width * 2)
+    y_positions = np.arange(len(books_name))
 
-    plt.bar(kraken_bar_x_pos, kraken_scores, color ='#4198D7', width = bars_width, label ='Kraken')
-    plt.bar(paddleocr_bar_x_pos, paddleocr_scores, color ='#46D39A', width = bars_width, label ='PaddleOCR')
-    plt.bar(tesseract_bar_x_pos, tesseract_scores, color ='#E55759', width = bars_width, label ='Tesseract')
+    plt.barh(y_positions - bars_width, kraken_scores, color='#4198D7', height=bars_width, label='Kraken')
+    plt.barh(y_positions, paddleocr_scores, color='#46D39A', height=bars_width, label='PaddleOCR')
+    plt.barh(y_positions + bars_width, tesseract_scores, color='#E55759', height=bars_width, label='Tesseract')
 
-    plt.xlabel('books', fontsize = 15)
-    plt.ylabel('scores', fontsize = 15)
-    plt.xticks(kraken_bar_x_pos, books_name, rotation='vertical', fontsize=10, ha='right', wrap=True)
-    plt.subplots_adjust(0.03, 0.27, 0.99, 0.99)
-
-    plt.legend()
+    plt.xlabel('OCR Accuracy Scores', fontsize=15)
+    plt.ylabel('Books', fontsize=15)
+    plt.yticks(y_positions, books_name, fontsize=10)
+    plt.title("OCR Accuracy Scores by Book and Framework", fontsize=16)
+    plt.legend(title="OCR Frameworks")
+    plt.gca().invert_yaxis()
+    plt.tight_layout()
+    plt.subplots_adjust(left=0.35)
     plt.show()
 
 generate_ocr_books_scores_comparison_chart(books_name, kraken_scores, paddleocr_scores, tesseract_scores)
