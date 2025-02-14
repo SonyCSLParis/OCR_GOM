83cbdf547c7cb7704b3c981e539f8fe6ea076fec
yanishk <yanis.harkouk@epitech.eu>
2025-02-14 10:56:19 +0100

    correct ocr errors with mistral

--- find_kraken_errors.py
+++ find_kraken_errors.py
@@ -1,16 +1,17 @@
 from utils import get_file_content, get_json_file_content, write_content_in_file, string_contains_digit
+from correct_with_mistral import retrieve_mistral_corrected_words
 import unicodedata
 import spacy
 
 def normalize_words(text):
     return unicodedata.normalize('NFC', text)
 
-def put_error_in_list(error_list, error, erorr_sentence):
+def put_error_in_list(error_list, error, error_sentence, sentence_index):
         for error_in_list in error_list:
                 if error_in_list[0] == error:
                         error_in_list[2] += 1
                         return error_list
-        return error_list.append([error, erorr_sentence, 0])
+        return error_list.append([error, error_sentence, 0, sentence_index])
 
 def is_in_dictionary(searched_word, dictionary):
         dictionary = dictionary.split("\n")
@@ -41,32 +42,47 @@ def is_in_one_dictionary(token_text):
             return result
         return compound_word_is_in_word_dictionary(token_text.lower(), morphalou_dictionary)
 
-def retrieve_kraken_errors(kraken_sentences):
-        kraken_errors = []
-        for index, kraken_sentence in enumerate(kraken_sentences):
-                for token in kraken_sentence:
+def retrieve_ocr_errors(ocr_sentences):
+        ocr_errors = []
+        for index, ocr_sentence in enumerate(ocr_sentences):
+                for token in ocr_sentence:
                         if token.is_punct == False:
                                 if (string_contains_digit(token.text)):
                                         continue
                                 result = is_in_one_dictionary(token.text)
                                 if (result[0] == False):
-                                        put_error_in_list(kraken_errors, token.text, kraken_sentence)
-        return kraken_errors
+                                        put_error_in_list(ocr_errors, token.text, ocr_sentence, index)
+        return ocr_errors
 
-def print_kraken_errors(kraken_errors):
-        for error in kraken_errors:
-                if error[2] == 0:
-                        print(error[0], " | ", error[1])
+def retrieve_ocr_errors_with_context(ocr_errors):
+        ocr_errors_with_context = ""
+        nb_ocr_errors_with_context = 0
+        for ocr_error in ocr_errors:
+                if ocr_error[2] == 0:
+                        ocr_errors_with_context += str(ocr_error[0]) + " | " + str(ocr_error[1]) + "\n"
+                        nb_ocr_errors_with_context += 1
+        return ocr_errors_with_context, nb_ocr_errors_with_context
 
-kraken_text = get_file_content("data/results/pages_concatenate/kraken_results_txt/L_école_du_jardin_potager/L_école_du_jardin_potager.txt")
-expected_text = get_file_content("data/results/pages_concatenate/expected_results_txt/L_école_du_jardin_potager/L_école_du_jardin_potager.txt")
+def correct_ocr_errors_in_text(ocr_sentences, ocr_errors, corrected_words):
+        corrected_word_index = 0
+        for ocr_error in ocr_errors:
+                if ocr_error[2] != 0:
+                        continue
+                ocr_sentences = ocr_sentences.replace(ocr_error[0], corrected_words[corrected_word_index])
+                corrected_word_index += 1
+        return ocr_sentences[0]
+
+ocr_text = get_file_content("data/results/pages_concatenate/kraken_results_txt/L_école_du_jardin_potager/L_école_du_jardin_potager.txt")
 french_words_dictionary = get_file_content("data/dictionary/fr.txt")
 french_cities_dictionary = get_file_content("data/dictionary/french_cities.txt")
 countries_dictionary = get_file_content("data/dictionary/countries.txt")
 morphalou_dictionary = get_file_content("data/dictionary/morphalou.txt")
 
 nlp = spacy.load("fr_dep_news_trf")
-kraken_text = normalize_words(kraken_text)
-kraken_sentences = [i for i in nlp(kraken_text).sents]
-kraken_errors = retrieve_kraken_errors(kraken_sentences)
-print_kraken_errors(kraken_errors)
+ocr_text = normalize_words(ocr_text)
+ocr_sentences = [i for i in nlp(ocr_text).sents]
+ocr_errors = retrieve_ocr_errors(ocr_sentences)
+ocr_errors_with_context, nb_ocr_errors_with_context = retrieve_ocr_errors_with_context(ocr_errors)
+corrected_words = retrieve_mistral_corrected_words(ocr_errors_with_context, nb_ocr_errors_with_context)
+corrected_text = correct_ocr_errors_in_text(str(ocr_sentences), ocr_errors, corrected_words)
+write_content_in_file("data/results/pages_concatenate/mistral_corrected_results_txt/L_école_du_jardin_potager/L_école_du_jardin_potager.txt", corrected_text, 'w')
